96783d3cbe1678f8ed481c2e91adfbf7201f079a
Cubernet <luxaole@gmail.com>
2015-04-17 13:58:00 +0800

    path to /blog

---
+++ README.md
@@ -0,0 +1,7 @@
+本博客运行于 [hexo](http://hexo.io) @ [GitHub](https://github.com/cubernet)
+
+Cubernet的技术博客
+
+私人空间，随意写写，欢迎评论，欢迎转载。
+
+如果你喜欢我的博客模板，可以自由克隆使用，不过希望可以添加链接，注明出处，谢谢:)
---
+++ blog/publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo -n "Enter comment:"
+read  comment
+files=`git status | grep deleted | awk '{print \$2}'`
+if [ "${files}" == "" ]; then
+   echo "nothing to remove"
+else
+	for var in $files
+	do
+		git rm $var
+	done
+fi
+git add .
+git commit -m "$comment"
+git push origin master
+exit 0
